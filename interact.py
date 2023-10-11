c21e9eb327bec0d308581e1d53fd90c44267be97
yewe <1904804171@qq.com>
2023-10-11 12:00:41 +0800

    添加了消息存储本地功能

--- interact.py
+++ interact.py
@@ -1,12 +1,28 @@
-# 下面这个类，以两个人之间的聊天为对象，其中u1,u2,指的是两个用户的用户名，message则是聊天记录
+import os
 
+
+# 下面这个类，以两个人之间的聊天为对象，其中u1,u2,指的是两个用户的用户名，message则是聊天记录
 class Interact(object):
     def __init__(self, a1, a2):
         self.u1 = a1
         self.u2 = a2
         self.messages = []
+        if not os.path.exists("./messages"):
+            os.mkdir("./messages")
+        else:
+            if os.path.exists("./messages/"+self.u1+self.u2+".txt"):
+                doc = open("./messages/"+self.u1+self.u2+".txt", "r")
+                news = doc.readlines()
+                for line in news:
+                    if ":" in line:
+                        sender, information = line.split(":")
+                        self.add_information(sender, information[:-1])
+                doc.close()
+# 生成存放消息的文件夹
 
     def add_information(self, sender, information):
+        if len(self.messages) >= 100:
+            self.messages.pop(0)
         self.messages.append([sender, information])
 # 添加聊天记录，可设置一个记录消息的上限，超过上限的部分可以删除
 
@@ -17,21 +33,23 @@ class Interact(object):
     def show(self):
         n = 0
         while n != len(self.messages):
-            print(self.messages[n][0], ':', self.messages[n][1], '\n')
+            print(self.messages[n][0], ':', self.messages[n][1])
             n += 1
 # 显示聊天记录，暂时用于程序测试
 
-
-# 用户，调试程序用
-class User(object):
-    def __init__(self, name, code):
-        self.name = name
-        self.code = code
+    def close(self):
+        doc = open(file="./messages/" + self.u1 + self.u2 + ".txt", mode="w")
+        while len(self.messages):
+            doc.write(self.messages[0][0] + ":" + self.messages[0][1] + "\n")
+            self.messages.pop(0)
+        doc.close()
 
 
 Try = Interact("user1", "user2")
+Try.show()
 first = input()
 while first != '0':
     Try.add_information(Try.u1, first)
     first = input()
 Try.show()
+Try.close()
