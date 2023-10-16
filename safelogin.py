29ad3681222bd58e3f648ec62b0606805dad6e7b
TonyFan1201 <147251240+TonyFan1201@users.noreply.github.com>
2023-10-16 20:16:39 +0800

    加了注册

--- safelogin.py
+++ safelogin.py
@@ -1,49 +1,58 @@
 import tkinter as tk
 import tkinter.messagebox
 import store
+import Zhuce
+
+user = ""
 
 
-# 登陆窗口
-loginRoot = tk.Tk()
-loginRoot.title("登录")
-loginRoot["height"] = 250
-loginRoot["width"] = 400
-loginRoot.resizable(0, 0)  # 限制窗口大小
+def zhuce(*args):
+    print("注册")
+    loginRoot.destroy()
+    Zhuce.create_zhuceroot()
+    create_loginroot()
 
-User = tk.StringVar()
-User.set("")
-password = tk.StringVar()
-password.set("")
+def create_loginroot():
+    # 登陆窗口
+    global loginRoot
+    loginRoot = tk.Tk()
+    loginRoot.title("登录")
+    loginRoot["height"] = 250
+    loginRoot["width"] = 400
+    loginRoot.resizable(0, 0)  # 限制窗口大小
 
-# 服务器标签
-labelUser = tk.Label(loginRoot, text='用户名', font=("宋体", 12))
-labelUser.place(x=60, y=70, width=100, height=30)
+    User = tk.StringVar()
+    User.set("")
+    password = tk.StringVar()
+    password.set("")
 
-entryUser = tk.Entry(loginRoot, width=80, textvariable=User, font=12)
-entryUser.place(x=160, y=70, width=130, height=30)
+    # 用户名标签
+    labelUser = tk.Label(loginRoot, text='用户名', font=("宋体", 12))
+    labelUser.place(x=60, y=70, width=100, height=30)
 
-# 用户名标签
-labelpassword = tk.Label(loginRoot, text='密码', font=("宋体", 12))
-labelpassword.place(x=70, y=110, width=80, height=30)
+    entryUser = tk.Entry(loginRoot, width=80, textvariable=User, font=12)
+    entryUser.place(x=160, y=70, width=130, height=30)
 
-entrypassword = tk.Entry(loginRoot, width=80, textvariable=password, font=15)
-entrypassword.place(x=160, y=110, width=130, height=30)
+    # 密码标签
+    labelpassword = tk.Label(loginRoot, text='密  码', font=("宋体", 12))
+    labelpassword.place(x=70, y=110, width=80, height=30)
 
+    entrypassword = tk.Entry(loginRoot, width=80, textvariable=password, font=15)
+    entrypassword.place(x=160, y=110, width=130, height=30)
 
-# 登录按钮
-def login(*args):
-    global user, Password
-    user = entryUser.get()
-    Password = entrypassword.get()
-    if user and Password:
-        if len(Password) > 16:
-            tkinter.messagebox.showerror('温馨提示', message='密码不能超过16位！')
-            password.set("")
-        else:
-            result = store.check_user(user,Password)
+    # 登录按钮
+    def login(*args):
+        global user, Password
+        user = entryUser.get()
+        Password = entrypassword.get()
+        if user and Password:
+            # if len(Password) > 16:
+            # tkinter.messagebox.showerror('温馨提示', message='密码不能超过16位！')
+            # password.set("")
+            # else:
+            result = store.check_user(user, Password)
             if result == 1:
-                tkinter.messagebox.showinfo('提示', message='注册并登录成功！')
-                loginRoot.destroy()  # 关闭窗口
+                tkinter.messagebox.showerror('提示', message='您还未注册！')
             elif result == 2:
                 tkinter.messagebox.showinfo('提示', message='登录成功！')
                 loginRoot.destroy()
@@ -51,16 +60,17 @@ def login(*args):
                 tkinter.messagebox.showerror('提示', message='密码错误！')
                 password.set("")
 
-    else:
-        tkinter.messagebox.showerror('温馨提示', message='用户名或密码为空！')
+        else:
+            tkinter.messagebox.showerror('温馨提示', message='用户名或密码为空！')
 
+    loginRoot.bind('<Return>', login)  # 回车绑定登录功能
+    but = tk.Button(loginRoot, text='登录', command=login, font=15)
+    but.place(x=100, y=160, width=70, height=30)
+    but2 = tk.Button(loginRoot, text='注册', command=zhuce, font=15)
+    but2.place(x=230, y=160, width=70, height=30)
+    loginRoot.mainloop()
 
+create_loginroot()
 
-
-
-loginRoot.bind('<Return>', login)            # 回车绑定登录功能
-but = tk.Button(loginRoot, text='登录', command=login, font=15)
-but.place(x=165, y=160, width=70, height=30)
-tkinter.mainloop()
 
 
