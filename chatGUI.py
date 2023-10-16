7d22d47d64d0a9380d5797f814a4438b87f0f0fe
ZhouZhanbo <2225446624@qq.com>
2023-10-16 20:55:29 +0800

    用户列表加入滚动条

--- chatGUI.py
+++ chatGUI.py
@@ -9,7 +9,6 @@ listbox1 = []
 a = []
 entry = []
 
-
 # 聊天界面创建
 def create_chatGUI():
     global user, root, listbox, listbox1, a, entry
@@ -27,11 +26,17 @@ def create_chatGUI():
     listbox.tag_config('blue', foreground='blue')
     listbox.tag_config('green', foreground='green')
     listbox.tag_config('pink', foreground='pink')
-    listbox.insert(tk.END, '欢迎加入聊天室 ！', 'blue')
+    listbox.insert(tk.END, '欢迎加入聊天室(默认为群聊) ！', 'blue')
 
-    # 创建多行文本框, 显示在线用户
+    # 列表动，滚动条跟着动
     listbox1 = tk.Listbox(root)
+    # 创建多行文本框, 显示在线用户
     listbox1.place(x=445, y=0, width=130, height=320)
+    # 创建用户列表的滚动条
+    sc1 = tk.Scrollbar(listbox1)
+    sc1.pack(side=tk.RIGHT,  fill=tk.Y)
+    # 滚动条动，列表跟着动
+    sc1.config(command=listbox1.yview)
 
     # 创建输入文本框和关联变量
     a = tk.StringVar()
