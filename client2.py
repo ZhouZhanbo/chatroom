9ce8d90c5e3c947fc2e1f8260e2514125e22a990
ZhouZhanbo <2225446624@qq.com>
2023-10-16 23:41:28 +0800

    修改了收方为all_user时，标红为个体而不是all_user的bug

--- client2.py
+++ client2.py
@@ -18,10 +18,16 @@ def recv():
                 chat.show_users(data["user_list"])
             elif data["type"] == "message":  # 接收的消息是文本消息
                 chat.revc(data["receiver"], data["sender"], data["message"])
-                for i in range(-1,chatGUI.listbox1.size()):  # 来消息时提醒：发送方背景变红
-                    if chatGUI.listbox1.get(i) == data["sender"] \
-                            and chatGUI.listbox1.curselection()[0] != i:  # 当已经选中该发方则不变红
-                        chatGUI.listbox1.itemconfigure((i,), bg="red")
+                if data["receiver"] != "all_user":
+                    for i in range(-1,chatGUI.listbox1.size()):  # 消息发送方背景变红
+                        if chatGUI.listbox1.get(i) == data["sender"] \
+                                and chatGUI.listbox1.curselection()[0] != i:  # 当已经选中该发方则不变红
+                            chatGUI.listbox1.itemconfigure((i,), bg="red")
+                else:  # 如果收方为all_user,则不能将sender标红，而是把all_user标红
+                    for i in range(-1,chatGUI.listbox1.size()):  # 消息发送方背景变红
+                        if chatGUI.listbox1.get(i) == "all_user" \
+                                and chatGUI.listbox1.curselection()[0] != i:  # 当已经选中该发方则不变红
+                            chatGUI.listbox1.itemconfigure((i,), bg="red")
         except:
             print("已经断开链接")
             break
